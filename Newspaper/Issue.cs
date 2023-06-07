b2f30f25698cc200d53f106253c430568f6f1f42
jkimball54 <jeremyck54@gmail.com>
2023-06-07 15:03:45 -0600

    added Issue Methods

--- Newspaper/Issue.cs
+++ Newspaper/Issue.cs
@@ -11,5 +11,26 @@ namespace Newspaper
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public List<Article> Articles { get; set; }
+
+
+        public int ArticleCount(NewspaperContext context)
+        {
+            var articleList = context.Articles.Where(article => article.Issue.Id == Id);
+            return articleList.Count();
+        }
+        public int ReporterCount(NewspaperContext context)
+        {
+            var articleList = context.Articles.Where(article => article.Issue.Id == Id);
+            var reporterList = articleList.Select(a => a.Reporter).ToList();
+            return reporterList.Distinct().Count();
+        }
+        public List<string> ReporterNames(NewspaperContext context)
+        {
+
+            var articleList = context.Articles.Where(article => article.Issue.Id == Id);
+            List<string> reporterNameList = articleList.Select(a => a.Reporter.Name).ToList();
+            List<string> uniqueReporterList = reporterNameList.Distinct();
+            return uniqueReporterList;
+        }
     }
 }
